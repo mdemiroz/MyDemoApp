3c8b4046e761d8f64d8d29e56477762682eefd1b
mdemiroz1 <murat.sady31@gmail.com>
2017-09-25 21:46:03 +0300

    Adding text about automatic deployment

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -38,21 +38,21 @@ public class AppTest extends TestCase{
 
     public void testFound(){
        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-       assertTrue(new App().counter(array,4));
+       assertTrue(new App().counter(array,4,1));
     }
 
     public void testNotFound(){
        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-       assertFalse(new App().counter(array,5));
+       assertFalse(new App().counter(array,5,0));
     }
 
     public void testEmptyArray(){
        ArrayList<Integer> array = new ArrayList<>();
-       assertFalse(new App().counter(array,1));
+       assertFalse(new App().counter(array,1,0));
     }
 
     public void testNull(){
-       assertFalse(new App().counter(null,1));
+       assertFalse(new App().counter(null,1,0));
     }
 
 }
